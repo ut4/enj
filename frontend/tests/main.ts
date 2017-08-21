bbc8fde7814a5eb29ac9322031472a1cfade7242
ut4 <ut4.l@live.fi>
2017-08-21 13:44:36 +0300

    Implementoi statistiikkanäkymän kehitys-alinäkymä.

--- frontend/tests/main.ts
+++ frontend/tests/main.ts
@@ -13,6 +13,7 @@ import 'tests/offline/OfflineStartViewTests';
 import 'tests/offline/OfflineTests';
 import 'tests/offline/SyncBackendTests';
 import 'tests/serviceworker/SWManagerTests';
+import 'tests/stat/StatProgressViewTests';
 import 'tests/stat/StatStrengthViewTests';
 import 'tests/stat/StatViewTests';
 import 'tests/ui/TimerTests';
