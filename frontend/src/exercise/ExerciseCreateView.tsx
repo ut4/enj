2e2f6a7c8e45a29f8757ff59cd66cdb02fbe53f0
ut4 <ut4.l@live.fi>
2017-10-27 10:24:54 +0300

    Lisää HomeView. Muokkaa applikaation värit energisemmiksi.

--- frontend/src/exercise/ExerciseCreateView.tsx
+++ frontend/src/exercise/ExerciseCreateView.tsx
@@ -7,7 +7,7 @@ import ExerciseForm from 'src/exercise/ExerciseForm';
 class ExerciseCreateView extends Component<any, any> {
     public render() {
         return <div>
-            <h2>Luo uusi liike</h2>
+            <h2>Luo uusi treeniliike</h2>
             <ExerciseForm exercise={ {name: '', variants: []} } afterInsert={ () => null }/>
         </div>;
     }
--- frontend/src/home/HomeView.tsx
+++ frontend/src/home/HomeView.tsx
@@ -1,8 +1,25 @@
 import Component from 'inferno-component';
 
+/**
+ * Applikaation kotinäkymä/dashboard.
+ */
 class HomeView extends Component<any, any> {
     public render() {
-        return <div>Kotinäkymä tulossa.</div>;
+        return <div class="home-view">
+            <h2>Dashboard</h2>
+            <div class="row">
+                <div class="col-6"><a href="#/statistiikka/voima" class="icon-button statistics">Kehitys &amp; statistiikka</a></div>
+                <div class="col-6"><a href="#/treenihistoria" class="icon-button line-chart">Parhaat sarjat</a></div>
+            </div>
+            <div class="row">
+                <div class="col-6"><a href="#/ohjelmat/luo-uusi" class="icon-button schedule">Luo ohjelma</a></div>
+                <div class="col-6"><a href="#/liikkeet/luo-uusi" class="icon-button benchpress">Luo liike</a></div>
+            </div>
+            <div class="row">
+                <div class="col-6"><a href="#/profiili" class="icon-button user-squared">Päivitä tietoja</a></div>
+                <div class="col-6"><a href="#/aloita-offline" class="icon-button offline">Aloita offline-mode</a></div>
+            </div>
+        </div>;
     }
 }
 
