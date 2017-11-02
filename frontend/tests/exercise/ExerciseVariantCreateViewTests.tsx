dcabdad4de6aad39f1fabecd46158f5e2df4d1c4
ut4 <ut4.l@live.fi>
2017-11-02 11:02:23 +0200

    Lisää POST /exercise & /exercise/variant-dataan userId, ettei se jää puut- tumaan GET /exercise-datasta offline-tilassa.

--- frontend/tests/exercise/ExerciseVariantCreateViewTests.tsx
+++ frontend/tests/exercise/ExerciseVariantCreateViewTests.tsx
@@ -3,31 +3,42 @@ import sinon from 'sinon';
 import * as itu from 'inferno-test-utils';
 import utils from 'tests/utils';
 import ExerciseBackend from 'src/exercise/ExerciseBackend';
+import UserState from 'src/user/UserState';
 import ExerciseVariantCreateView from 'src/exercise/ExerciseVariantCreateView';
 import iocFactories from 'src/ioc';
 import exerciseTestUtils from 'tests/exercise/utils';
 
 QUnit.module('exercise/ExerciseVariantCreateView', hooks => {
     let testDropdownExercises: Array<Enj.API.Exercise>;
-    let exerciseBackendIocOverride: sinon.SinonStub;
     let shallowExerciseBackend: ExerciseBackend;
+    let exerciseBackendIocOverride: sinon.SinonStub;
+    let shallowUserState: UserState;
+    let userStateIocOverride: sinon.SinonStub;
     hooks.beforeEach(() => {
         testDropdownExercises = exerciseTestUtils.getSomeDropdownExercises();
         shallowExerciseBackend = Object.create(ExerciseBackend.prototype);
         exerciseBackendIocOverride = sinon.stub(iocFactories, 'exerciseBackend').returns(shallowExerciseBackend);
+        shallowUserState = Object.create(UserState.prototype);
+        userStateIocOverride = sinon.stub(iocFactories, 'userState').returns(shallowUserState);
     });
     hooks.afterEach(() => {
         exerciseBackendIocOverride.restore();
+        userStateIocOverride.restore();
     });
-    QUnit.test('lähettää tiedot backendiin', assert => {
-        const insertCallStub = sinon.stub(shallowExerciseBackend, 'insertVariant').returns(Promise.resolve(1));
+    QUnit.test('lähettää uuden treenivariantin userId:llä backendiin', assert => {
+        const testUserId = 'uuid';
+        const userStateReadStub = sinon.stub(shallowUserState, 'getUserId').returns(Promise.resolve(testUserId));
         const exerciseListFetch = sinon.stub(shallowExerciseBackend, 'getAll').returns(Promise.resolve(testDropdownExercises));
+        const insertCallStub = sinon.stub(shallowExerciseBackend, 'insertVariant').returns(Promise.resolve(1));
+        const expectedNewVariant = {content: 'foo', exerciseId: testDropdownExercises[0].id, userId: testUserId};
         //
         const rendered = itu.renderIntoDocument(<ExerciseVariantCreateView/>);
-        const expectedNewVariant = {content: 'foo', exerciseId: testDropdownExercises[0].id};
-        // Odota, että liikelista latautuu
+        // Odota, että handleMount luo uuden variantin
         const done = assert.async();
-        exerciseListFetch.firstCall.returnValue.then(() => {
+        userStateReadStub.firstCall.returnValue.then(() => {
+        // Odota, että liikelista latautuu
+            return exerciseListFetch.firstCall.returnValue;
+        }).then(() => {
             // Täytä & lähetä lomake
             const variantContentInput = exerciseTestUtils.getContentInput(rendered);
             const newVariantContent = expectedNewVariant.content;
